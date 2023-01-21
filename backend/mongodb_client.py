2e8e4bda9d22cc8d040158fde2027fc9be9e7d65
PhysBoom <mc_gold05@yahoo.com>
2023-01-21 14:13:59 -0800

    Added database interactor

---
+++ backend/mongodb_client.py
@@ -0,0 +1,39 @@
+import pymongo
+import os
+
+
+class Database:
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(
+            os.environ.get("MONGODB_URI"), uuidRepresentation="standard"
+        )
+        Database.DATABASE = client[
+            "Testing" if os.environ.get("ENV") == "dev" else "Production"
+        ]
+
+    @staticmethod
+    def insert_one(collection, data):
+        Database.DATABASE[collection].insert_one(data)  # type: ignore
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)  # type: ignore
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)  # type: ignore
+
+    @staticmethod
+    def update_one(collection, query, data):
+        Database.DATABASE[collection].update_one(query, data, upsert=True)  # type: ignore
+
+    @staticmethod
+    def remove(collection, query):
+        Database.DATABASE[collection].remove(query)  # type: ignore
+
+    @staticmethod
+    def aggregate(collection, pipeline):
+        return Database.DATABASE[collection].aggregate(pipeline)  # type: ignore
