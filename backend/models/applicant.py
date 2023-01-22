c618776e2c4272303962a9dbdb7b54c8042b3432
nathankang413 <nathankang413@gmail.com>
2023-01-22 10:13:22 -0800

    random applicant generator

--- backend/models/applicant.py
+++ backend/models/applicant.py
@@ -1,3 +1,5 @@
+import random
+import randominfo
 from pydantic import Field
 
 from controllers.mongodb_client import Database
@@ -7,6 +9,7 @@ from .bio import Bio
 from .user import User, UserTypes
 from controllers.firebase_controller import upload_file_to_firebase_storage
 
+from previous_job import PreviousJob
 
 class Applicant(User):
     bio: Bio = Field(default=None)  # TODO: Implement bio
@@ -58,3 +61,31 @@ class Applicant(User):
                 "users", {"user_type": UserTypes.APPLICANT.value}
             )
         ]
+
+
+    degree_types = ['Associates Degree', 'Bachelors Degree', 'Doctorate']
+    majors = ['Computer Science', 'Communications', 'Political Science', 'Business', 'Economics', 'English', 'Psychology', 'Nursing', 'Chemical Engineering', 'Biology', 'History', 'Social Sciences', 'Engineering', 'Journalism', 'Education']
+    universities = ['Princeton University', 'Massachusetts Institute of Technology', 'Harvard University', 'Stanford University', 'Yale University', 'University of Chicago', 'Johns Hopkins University', 'University of Pennsylvania', 'California Institute of Technology', 'Duke University', 'Northwestern University', 'Dartmouth College', 'Brown University', 'Vanderbilt University', 'Rice University', 'Cornell Univerity', 'Columbia University', 'California Polytechnic State University, SLO', 'University of California, Berkeley', 'University of California, LA', 'Carnegie Mellon University', 'Emory University', 'Georgetown Universtiy', 'New York University', 'University of Michigan', 'Unversity of Southern California', 'University of Virginia']
+    skills = ['adaptability', 'attention to detail', 'creativity', 'decision making', 'work ethic', 'time management', 'data analysis', 'event planning', 'food preparation', 'graphic design', 'typing', 'interpersonal communication', 'public speaking', 'teamwork', 'technical writing', 'training', 'verbal communication', 'written communication', 'email management', 'research', 'social media', 'web design', 'marketing', 'patience', 'sales', 'troubleshooting', 'conflict resolution', 'delegation', 'mentorship', 'motivation', 'team management', 'creativity']
+
+
+    @staticmethod
+    def create_random_applicant():
+        resume = Resume()
+        resume.degree_type = ', '.join(random.choices(Applicant.degree_types)[0], random.choices(Applicant.majors)[0])
+        resume.university = random.choices(Applicant.universities)[0]
+        resume.gpa = random.uniform(2, 4)
+        resume.grad_date = random.choices([random.randrange(948591705, 1674438105), -1], cum_weights=[95, 5])[0]
+        resume.skills = random.choices(Applicant.skills)[0:random.randrange(3, 10)]
+        resume.employment_history = []
+        for _ in range(random.randrange(0, 4)):
+            resume.employment_history.append(PreviousJob.random_job_for_resume())
+
+        applicant = Applicant()
+        info = randominfo.Person()
+        applicant.name = info.full_name
+        applicant.email = info.email
+        applicant.phone_number = info.phone
+        applicant.resume = resume
+
+        return applicant
--- backend/models/previous_job.py
+++ backend/models/previous_job.py
@@ -1,3 +1,4 @@
+import random
 from pydantic import BaseModel, Field
 
 
@@ -8,3 +9,17 @@ class PreviousJob(BaseModel):
         default=-1
     )  # UNIX timestamp to the nearest day, -1 = still employed
     job_title: str = Field(...)
+
+    # job_titles = ['Accountant', 'Accounting Clerk', 'Controller', 'Office manager', 'Administrative Assistant', 'Receptionist', 'Internal Help Desk Technician,' 'IT Manager', 'HR Coordinator', 'HR Manager', 'Recruiting Manager', 'Health and Safety Manager', 'Sales Representative, Account Manager', 'Sales Manager', 'Sales Director', 'Sales VP', 'Customer Service Representative', 'Technical Customer Support Specialist', 'Customer Service Manager', 'Graphic Designer', 'Marketing Coordinator', 'Product Manager', 'Marketing Manager', 'Marketing Director', 'Chief Executive Officer', 'Chief Operating Officer', 'Chief Financial Officer', 'Chief Information Officer', 'Chief Technology Officer', 'Chief Marketing Officer']
+    companies = ['Luxury Boutique', 'Best Restaurant', 'Smart Tech', 'Math Accounting', 'Smart Consulting', 'Tall Bar', 'Beautiful Art', 'Clear Crystal', '102 Kitchen', 'Smooth Skincare Salon', 'Crazy Coffee', 'Shiny Jewelry', 'Fishi Sushi', 'Thrifty Thrift Shop', 'Battle Bar Arcade', 'Rolling Bikes', 'Yumme Cafe', 'Pause Records']
+
+    @staticmethod
+    def random_job_for_resume():
+        with open('jobs.txt', 'r') as file:
+            job_titles = file.readlines()
+        job = PreviousJob()
+        job.title = random.choices(job_titles)[0]
+        job.employer = random.choices(PreviousJob.companies)[0]
+        job.start_date = random.randrange(948591705, 1674438105)
+        job.end_date = random.randrange(job.start_date, 1674438105)
+        return job
